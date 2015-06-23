3ec5a615edea47fcf08a01d08fecac1438b9424a
jpadams <jeremy@puppetlabs.com>
2015-06-23 10:23:32 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class foo {
   notify{"readying data from the yaml file":} 
   include 'stdlib' 
+  # calling my custom function included in this module
   prepyaml()
   $data = loadyaml('/tmp/itsclean.yaml')
   $thehost = $data["Host ID"] 
